5aa96e235a5fd41c028068fe44bbd395e80ba70e
subrasub <s35subra@edu.uwaterloo.ca>
2018-10-28 07:17:42 -0400

    API calls initiated

--- app.js
+++ app.js
@@ -8,11 +8,28 @@ var request = require('request'),
 var crypt = "http://www.mocky.io/v2/5bd1dfac2f00008d2ed8fe73"
 var url_id = "https://api.coinmarketcap.com/v2/ticker"
 
+app.set("view engine", "ejs")
 
-request(crypt, function(err, res, body){
-	var list_crypt = JSON.parse(body)
-	console.log(list_crypt) 
+app.get('/', function(req, res){
+	res.render('list.ejs')
 })
+
+app.get('/list', function(req, res){
+	request(crypt, function(err, rep , body){
+		var list_crypt = JSON.parse(body)
+		console.log(list_crypt) 
+
+		var data 
+		for(let i=0; i<list_crypt['currencies'].length; i++){
+			request(url_id+'/'+list_crypt['currencies'][i], function(err, res, body){
+				data = JSON.parse(body)
+				console.log(data)
+			})
+		}
+		res.render('list.ejs', {data: data})
+	})	
+})
+
 
 app.listen(8080, function(){
 	console.log("app is running!")
