2a9ed8f80e2b344064d49aea82670cf8cf68314b
subrasub <s35subra@edu.uwaterloo.ca>
2018-10-31 11:20:06 -0400

    working results

--- app.js
+++ app.js
@@ -6,7 +6,6 @@ var request = require('request'),
 
 
 var crypt = "http://www.mocky.io/v2/5bd1dfac2f00008d2ed8fe73"
-var url_id = "https://api.coinmarketcap.com/v2/ticker"
 
 app.set("view engine", "ejs")
 
@@ -21,20 +20,38 @@ app.get('/search', function(req, res, body){
 })
 
 app.get('/results', function(req, res){
-	request(crypt, function(err, rep , body){
-		var list_crypt = JSON.parse(body)
-		console.log(list_crypt) 
+	console.log("Results Page")
 
-		var data 
-		for(let i=0; i<list_crypt['currencies'].length; i++){
-			request(url_id+'/'+list_crypt['currencies'][i], function(err, res, body){
-				data = JSON.parse(body)
-				console.log(data)
-			})
+	var query = req.query.search.toLowerCase()
+	var key_id = 0
+	if(query === "bitcoin")
+		key_id = 1
+
+	var url = "https://api.coinmarketcap.com/v2/ticker/" + key_id + '/'
+	request(url, function(error, response, body){
+		if(!error && res.statusCode == 200){
+			var data = JSON.parse(body)
+			console.log(data)
+			res.render('list.ejs', {data: data})
 		}
-		res.render('list.ejs', {data: data})
-	})	
+	})
 })
+
+// app.get('/results', function(req, res){
+// 	request(crypt, function(err, rep , body){
+// 		var list_crypt = JSON.parse(body)
+// 		console.log(list_crypt) 
+
+// 		var data 
+// 		for(let i=0; i<list_crypt['currencies'].length; i++){
+// 			request(url_id+'/'+list_crypt['currencies'][i], function(err, res, body){
+// 				data = JSON.parse(body)
+// 				console.log(data)
+// 			})
+// 		}
+// 		res.render('list.ejs', {data: data})
+// 	})	
+// })
 
 
 app.listen(8080, function(){
