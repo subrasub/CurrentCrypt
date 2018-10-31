aad0ceb7f06b143ed3a93a0fa2371dd463d28835
subrasub <s35subra@edu.uwaterloo.ca>
2018-10-31 17:37:48 -0400

    created new routes

--- app.js
+++ app.js
@@ -1,22 +1,32 @@
-// https://api.coinmarketcap.com/v2/ticker
-// http://www.mocky.io/v2/5bd1dfac2f00008d2ed8fe73
+// API: https://api.coinmarketcap.com/v2/ticker
 var request = require('request'),
 	express = require('express'),
 	app 	= express()
 
-
-var crypt = "http://www.mocky.io/v2/5bd1dfac2f00008d2ed8fe73"
+var url = "https://api.coinmarketcap.com/v2/"
 
 app.set("view engine", "ejs")
 
 app.get('/', function(req, res){
-	res.render('list.ejs')
+	console.log("home page")
+	res.render('home')
+})
+
+app.get('/list', function(req, res){
+	console.log("list page")
+	request(url+'/ticker/?limit=10', function(error, response, body){
+		if(!error && res.statusCode == 200){
+			var data = JSON.parse(body)
+			// console.log(data)
+			res.render("list", {data:data})
+		}
+	})
 })
 
 app.get('/search', function(req, res, body){
 	console.log("search page")
 	var name = req.params.name
-	res.render("search.ejs")
+	res.render("search")
 })
 
 app.get('/results', function(req, res){
@@ -27,32 +37,15 @@ app.get('/results', function(req, res){
 	if(query === "bitcoin")
 		key_id = 1
 
-	var url = "https://api.coinmarketcap.com/v2/ticker/" + key_id + '/'
-	request(url, function(error, response, body){
+	
+	request(url +'/ticker/' + key_id + '/', function(error, response, body){
 		if(!error && res.statusCode == 200){
 			var data = JSON.parse(body)
 			console.log(data)
-			res.render('list.ejs', {data: data})
+			res.render('single', {data: data})
 		}
 	})
 })
-
-// app.get('/results', function(req, res){
-// 	request(crypt, function(err, rep , body){
-// 		var list_crypt = JSON.parse(body)
-// 		console.log(list_crypt) 
-
-// 		var data 
-// 		for(let i=0; i<list_crypt['currencies'].length; i++){
-// 			request(url_id+'/'+list_crypt['currencies'][i], function(err, res, body){
-// 				data = JSON.parse(body)
-// 				console.log(data)
-// 			})
-// 		}
-// 		res.render('list.ejs', {data: data})
-// 	})	
-// })
-
 
 app.listen(8080, function(){
 	console.log("app is running!")
